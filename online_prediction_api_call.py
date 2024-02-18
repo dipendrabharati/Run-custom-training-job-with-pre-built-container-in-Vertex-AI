bcbe69eeaeb1b1198d9379661dd41cded0f0864f
dipendrabharati <dibsbharati@gmail.com>
2024-02-17 19:45:37 -0800

    online prediction update

--- online_prediction_api_call.py
+++ online_prediction_api_call.py
@@ -2,18 +2,23 @@
 
 from google.cloud import aiplatform
 
+# endpoint_name  = projects/your-project-id/locations/us-central1/endpoints/your-endpoint-id
 endpoint = aiplatform.Endpoint(
-    endpoint_name="ENDPOINT_STRING"
+    endpoint_name="projects/rugged-research-400319/locations/us-central1/endpoints/550382535515832320"
 )
 
-# A test example we'll send to our model for prediction
-test_mpg = []
+# using total room as a feature
+# my_feature = [[1960], [3400], [3677], [2202], [2403], [5652], [3318], [2552], [1364], [3468]]
+# this is the format we are using while training the model (model.fit())
+my_feature = [[1960], [3400]]
 
-response = endpoint.predict([test_mpg])
+response = endpoint.predict(my_feature)
 
 print('API response: ', response)
 
-print('Predicted MPG: ', response.predictions[0][0])
+for i in range(len(my_feature)):
+    print("Number of rooms" + "   " + " Predicted House Price")
+    print( my_feature[i][0], "               ", response.predictions[i][0])
 
 # ENDPOINT=$(cat deploy-output.txt | sed -nre 's:.*Resource name\: (.*):\1:p' | tail -1)
 # sed -i "s|ENDPOINT_STRING|$ENDPOINT|g" predict.py
